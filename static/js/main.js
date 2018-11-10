68aadaf3ebe33ef0032bf4029a74af4bb34bf098
akhilanto <aanto@gmu.edu>
2018-11-09 19:22:06 -0500

    commit

---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+var htmlentities = {
+		encode : function(str) {
+			var buf = [];
+			
+			for (var i=str.length-1;i>=0;i--) {
+				buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
+			}
+			
+			return buf.join('');
+		},
+		decode : function(str) {
+			return str.replace(/&#(\d+);/g, function(match, dec) {
+				return String.fromCharCode(dec);
+			});
+		}
+	}
